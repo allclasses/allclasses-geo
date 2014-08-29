5285edf6320ef0756b82f6f6af4fd3bab8bd9204
Greg Thole <gthole@gmail.com>
2014-08-29 15:29:43 -0400

    Refactor service gets

--- geo/services/geocode.py
+++ geo/services/geocode.py
@@ -4,49 +4,47 @@ from geo.services.connections import get_tiger_db_conn
 
 class GeocodeService(object):
     def get(self, data):
-        if "location" not in data:
+        """
+        Geocodes a given location string and returns either a dictionary of it's
+        coordinates or None
+        """
+        if not isinstance(data.get("location"), basestring):
             raise AbortException(400, "location string required")
-        return geocode_tiger(data["location"])
 
+        query = "SELECT g.rating," \
+                " ST_X(g.geomout) AS longitude," \
+                " ST_Y(g.geomout) AS latitude," \
+                " (addy).address AS street_num," \
+                " (addy).streetname AS street_name," \
+                " (addy).streettypeabbrev AS street_type," \
+                " (addy).location AS city," \
+                " (addy).stateabbrev AS region," \
+                " (addy).zip AS postal_code " \
+                "FROM geocode(%s, 1) AS g;"
 
-def geocode_tiger(location_string):
-    """
-    Geocodes a given location string and returns either a dictionary of it's
-    coordinates or None
-    """
-    query = "SELECT g.rating," \
-            " ST_X(g.geomout) AS longitude," \
-            " ST_Y(g.geomout) AS latitude," \
-            " (addy).address AS street_num," \
-            " (addy).streetname AS street_name," \
-            " (addy).streettypeabbrev AS street_type," \
-            " (addy).location AS city," \
-            " (addy).stateabbrev AS region," \
-            " (addy).zip AS postal_code " \
-            "FROM geocode(%s, 1) AS g;"
+        # SQL implementing geocode() does not sanitize its inputs properly
+        rev_location_string = data["location"].replace(
+            '(', '\\(').replace(')', '\\)')
 
-    # SQL implementing geocode() does not sanitize its inputs properly
-    rev_location_string = location_string.replace(
-        '(', '\\(').replace(')', '\\)')
+        # Execute the geocode query
+        conn = get_tiger_db_conn()
+        cursor = conn.cursor()
+        cursor.execute(query, [rev_location_string])
+        result = cursor.fetchone()
 
-    # Execute the geocode query
-    conn = get_tiger_db_conn()
-    cursor = conn.cursor()
-    cursor.execute(query, [rev_location_string])
-    result = cursor.fetchone()
+        # Must match columns in "query", defined above
+        if result:
+            (rating, longitude, latitude, street_num, street_name, street_type,
+             city, region, postal_code) = result
 
-    # Must match columns in "query", defined above
-    if result:
-        (rating, longitude, latitude, street_num, street_name, street_type,
-         city, region, postal_code) = result
-
-        if latitude and longitude:
-            return {
-                "rating": rating,
-                "street": "%s %s %s" % (street_num, street_name, street_type),
-                "city": city,
-                "region": region,
-                "postal_code": postal_code,
-                "latitude": latitude,
-                "longitude": longitude
-            }
+            if latitude and longitude:
+                return {
+                    "rating": rating,
+                    "street": "%s %s %s" % (street_num, street_name,
+                                            street_type),
+                    "city": city,
+                    "region": region,
+                    "postal_code": postal_code,
+                    "latitude": latitude,
+                    "longitude": longitude
+                }
--- geo/services/reverse.py
+++ geo/services/reverse.py
@@ -4,46 +4,47 @@ from geo.services.connections import get_tiger_db_conn
 
 class ReverseGeocodeService(object):
     def get(self, data):
+        """
+        Reverse geocodes the lat/lon supplied in a dictionary and returns
+        an address dict or None
+        """
         if not (isinstance(data.get("latitude"), float) and
                 isinstance(data.get("longitude"), float)):
             raise AbortException(400, "lat/lon must be floats")
-        return reverse_geocode_tiger(data["latitude"], data["longitude"])
 
+        latitude = data["latitutde"]
+        longitude = data["longitude"]
 
-def reverse_geocode_tiger(latitude, longitude):
-    """
-    Geocodes a given location string and returns either a dictionary of it's
-    coordinates or None
-    """
-    query = "SELECT " \
-            " r.addy[1].address AS street_num," \
-            " r.addy[1].streetname AS street_name," \
-            " r.addy[1].streettypeabbrev AS street_type," \
-            " r.addy[1].location AS city," \
-            " r.addy[1].stateabbrev AS region," \
-            " r.addy[1].zip AS postal_code " \
-            "FROM reverse_geocode(" \
-            "ST_GeomFromText('POINT(%s %s)', 4269), true) " \
-            "AS r;"
+        query = "SELECT " \
+                " r.addy[1].address AS street_num," \
+                " r.addy[1].streetname AS street_name," \
+                " r.addy[1].streettypeabbrev AS street_type," \
+                " r.addy[1].location AS city," \
+                " r.addy[1].stateabbrev AS region," \
+                " r.addy[1].zip AS postal_code " \
+                "FROM reverse_geocode(" \
+                "ST_GeomFromText('POINT(%s %s)', 4269), true) " \
+                "AS r;"
 
-    # Execute the geocode query
-    conn = get_tiger_db_conn()
-    cursor = conn.cursor()
-    cursor.execute(query, [longitude, latitude])
-    result = cursor.fetchone()
+        # Execute the geocode query
+        conn = get_tiger_db_conn()
+        cursor = conn.cursor()
+        cursor.execute(query, [longitude, latitude])
+        result = cursor.fetchone()
 
-    # Must match columns in "query", defined above
-    if result:
-        (street_num, street_name, street_type,
-         city, region, postal_code) = result
+        # Must match columns in "query", defined above
+        if result:
+            (street_num, street_name, street_type,
+             city, region, postal_code) = result
 
-        if street_num and street_name:
-            return {
-                "rating": None,
-                "street": "%s %s %s" % (street_num, street_name, street_type),
-                "city": city,
-                "region": region,
-                "postal_code": postal_code,
-                "latitude": latitude,
-                "longitude": longitude
-            }
+            if street_num and street_name:
+                return {
+                    "rating": None,
+                    "street": "%s %s %s" % (street_num, street_name,
+                                            street_type),
+                    "city": city,
+                    "region": region,
+                    "postal_code": postal_code,
+                    "latitude": latitude,
+                    "longitude": longitude
+                }
